fca6ebb884865d3bcddb4554d3d4c2c295aa8c9a
pavibhat13 <69513454+pavibhat13@users.noreply.github.com>
2021-06-11 19:15:59 +0530

    Initial build

---
+++ precache-manifest.9bb543f924f99156f36ada7a6fec689a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799f013c15550c10c0354d9eaa8aadbf",
+    "url": "/React-Spotify-Clone/index.html"
+  },
+  {
+    "revision": "8458da8cd3d088e2f9ba",
+    "url": "/React-Spotify-Clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8da5841ea8f157d93ea",
+    "url": "/React-Spotify-Clone/static/js/2.1b2ec151.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Spotify-Clone/static/js/2.1b2ec151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8458da8cd3d088e2f9ba",
+    "url": "/React-Spotify-Clone/static/js/main.332cfe9d.chunk.js"
+  },
+  {
+    "revision": "97483b2c759deb1aa9f3",
+    "url": "/React-Spotify-Clone/static/js/runtime-main.bfc05caa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Spotify-Clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
